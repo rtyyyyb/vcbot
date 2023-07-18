ff43ffa77ebc16c0c7c65ab76f1241abf591126b
Jason C <jason.cipriani@gmail.com>
2023-07-18 10:30:17 -0400

    remove extension loading code

--- main.py
+++ main.py
@@ -19,26 +19,13 @@ class CustomBot(commands.Bot):
     client: aiohttp.ClientSession
     _uptime: datetime.datetime = datetime.datetime.utcnow()
 
-    def __init__(self, prefix: str, ext_dir: str, *args: typing.Any, **kwargs: typing.Any) -> None:
+    def __init__(self, prefix: str, *args: typing.Any, **kwargs: typing.Any) -> None:
         intents = discord.Intents.default()
         intents.members = True
         intents.message_content = True
         super().__init__(*args, **kwargs, command_prefix=commands.when_mentioned_or(prefix), intents=intents)
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.ext_dir = ext_dir
         self.synced = False
-
-    async def _load_extensions(self) -> None:
-        if not os.path.isdir(self.ext_dir):
-            self.logger.error(f"Extension directory {self.ext_dir} does not exist.")
-            return
-        for filename in os.listdir(self.ext_dir):
-            if filename.endswith(".py") and not filename.startswith("_"):
-                try:
-                    await self.load_extension(f"{self.ext_dir}.{filename[:-3]}")
-                    self.logger.info(f"Loaded extension {filename[:-3]}")
-                except commands.ExtensionError:
-                    self.logger.error(f"Failed to load extension {filename[:-3]}\n{traceback.format_exc()}")
 
     async def on_error(self, event_method: str, *args: typing.Any, **kwargs: typing.Any) -> None:
         self.logger.error(f"An error occurred in {event_method}.\n{traceback.format_exc()}")
@@ -48,7 +35,6 @@ class CustomBot(commands.Bot):
 
     async def setup_hook(self) -> None:
         self.client = aiohttp.ClientSession()
-        await self._load_extensions()
         if not self.synced:
             await self.tree.sync()
             self.synced = not self.synced
@@ -286,7 +272,7 @@ def time():
 
 def main() -> None:
     logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
-    bot = CustomBot(prefix="!", ext_dir="cogs", activity=discord.Game(name='!help to learn more'))
+    bot = CustomBot(prefix="!", activity=discord.Game(name='!help to learn more'))
 
     @bot.command(aliases=['hi'])
     async def hello(ctx: commands.Context, *args):
